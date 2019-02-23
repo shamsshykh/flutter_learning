d121d9a03782fd40970805c85a1c91488e584278
Muhammad Shams <mr.shamssheikh@gmail.com>
2019-02-23 16:43:02 +0500

    learn scaffold,stateless widget, appbar and body widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyNewApp());
+
+class MyNewApp  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Welcome World"),
+          backgroundColor: Colors.amber,
+          brightness: Brightness.light,
+          textTheme: TextTheme(title: TextStyle(color: Colors.black,fontSize: 20,fontStyle: FontStyle.italic)),
+        ),
+        body: new Center(
+          child: new Text("Hellow World"),
+        ),
+      )
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,6 +48,7 @@ class RandomWordsState extends StatefulWidget {
 }
 
 class _RandomWordsStateState extends State<RandomWordsState> {
+
   @override
   Widget build(BuildContext context) {
     final wordPair=WordPair.random();
