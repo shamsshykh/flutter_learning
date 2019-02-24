dddbeb0057642da910a40f6072a942c936392b36
Muhammad Shams <mr.shamssheikh@gmail.com>
2019-02-24 21:50:01 +0500

    Learn splash screen or debug/released mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyStatefullApp());
+void main() {
+  Util.checkDebugMode();
+  runApp(MyStatefullApp());
+}
 
-class MyStatefullApp extends StatelessWidget{
+class Util {
+  static String url = "http:http:live";
+  static String mode = "release";
+
+  static checkDebugMode() {
+    assert(() {
+      mode = "debug";
+      url = "http:http:debug";
+      return true;
+    }());
+  }
+}
+
+class MyStatefullApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
       home: new _MyAppState(),
+      theme: ThemeData(
+          primarySwatch: Colors.amber,
+          brightness: Brightness.light,
+          accentColor: Colors.redAccent),
     );
   }
 }
@@ -20,20 +40,25 @@ class _MyAppState extends StatefulWidget {
 }
 
 class __MyAppStateState extends State<_MyAppState> {
-  int incremenent=0;
+  int incremenent = 0;
+
   Widget _bodyWidget() {
-   return  new Container(
-      child:  new Center(
+    return new Container(
+      child: new Center(
           child: new Column(
-            children: <Widget>[
-              new Text("Hellow World $incremenent"),
-              new RaisedButton(onPressed: _onClick,child: new Text("Click me")  ),
-            ],
-          )
-      ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Text("Hellow World $incremenent"),
+          new Text("This is ${Util.mode} Mode"),
+          new Text("${Util.url}"),
+          new RaisedButton(onPressed: _onClick, child: new Text("Click me")),
+        ],
+      )),
     );
   }
-  void _onClick(){
+
+  void _onClick() {
     setState(() {
       incremenent++;
     });
@@ -43,50 +68,53 @@ class __MyAppStateState extends State<_MyAppState> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Welcome Flutter",textAlign: TextAlign.center,),
+        title: new Text(
+          "Welcome Flutter",
+          textAlign: TextAlign.center,
+        ),
       ),
       body: _bodyWidget(),
     );
   }
-
 }
 
-
-
-class MyNewApp  extends StatelessWidget{
+class MyNewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Welcome World"),
-          backgroundColor: Colors.amber,
-          brightness: Brightness.light,
-          textTheme: TextTheme(title: TextStyle(color: Colors.black,fontSize: 20,fontStyle: FontStyle.italic)),
-        ),
-        body: new Center(
-          child: new Text("Hellow World"),
-        ),
-      )
-    );
+        home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Welcome World"),
+        backgroundColor: Colors.amber,
+        brightness: Brightness.light,
+        textTheme: TextTheme(
+            title: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontStyle: FontStyle.italic)),
+      ),
+      body: new Center(
+        child: new Text("Hellow World"),
+      ),
+    ));
   }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        title: "Welcome To Flutter",
-        home:  Scaffold(
-          appBar:  AppBar(
-            title:  Text("Wellcome To Flutter"),
-          ),
-          body:  Center(
-           // child: new Text(WordPair.random().asCamelCase),
-            child: new RandomWordsState(),
-          ),
+    return MaterialApp(
+      title: "Welcome To Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Wellcome To Flutter"),
         ),
+        body: Center(
+          // child: new Text(WordPair.random().asCamelCase),
+          child: new RandomWordsState(),
+        ),
+      ),
     );
   }
 }
@@ -97,11 +125,9 @@ class RandomWordsState extends StatefulWidget {
 }
 
 class _RandomWordsStateState extends State<RandomWordsState> {
-
   @override
   Widget build(BuildContext context) {
-    final wordPair=WordPair.random();
+    final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
 }
-
